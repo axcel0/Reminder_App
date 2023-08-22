de9e664fdd3e991d994c97378ae25f17bd3567b8
Axel Santoso <axcelsantoso0@gmail.com>
2023-08-22 15:36:44 +0700

    feat: edit data can show previous data, but the date is set to minDate.

--- app/src/main/java/com/example/myapplication/UI/EditActivity.kt
+++ app/src/main/java/com/example/myapplication/UI/EditActivity.kt
@@ -6,11 +6,9 @@ import android.widget.DatePicker
 import android.widget.TimePicker
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.R
-import com.example.myapplication.UI.MainActivity
 import com.example.myapplication.databinding.ActivityCreateBinding
-import com.example.myapplication.databinding.ActivityMainBinding
-import com.example.myapplication.models.AppDatabase
 import com.google.android.material.textfield.TextInputEditText
+import java.time.LocalDateTime
 
 
 class EditActivity : AppCompatActivity() {
@@ -28,23 +26,26 @@ class EditActivity : AppCompatActivity() {
         //bundle data
         val id = bundle?.getLong("id")
         val name = bundle?.getString("name")
-        val added = bundle?.getLong("added")
+        val dateTime = bundle?.getLong("added")
 
         val title = findViewById<TextInputEditText>(R.id.title)
-        val datePicker =  findViewById<DatePicker>(R.id.datePicker)
-        val timePicker =  findViewById<TimePicker>(R.id.timePicker)
+        val date =  findViewById<DatePicker>(R.id.datePicker)
+        val time =  findViewById<TimePicker>(R.id.timePicker)
 
         title.setText(name)
-        datePicker.minDate = System.currentTimeMillis().also { timePicker.setIs24HourView(true) }
-
+        //initiate date and time
+        date.minDate = System.currentTimeMillis().also { time.setIs24HourView(true) }
+        time.hour = LocalDateTime.ofEpochSecond(dateTime!!, 0, java.time.ZoneId.systemDefault().rules.getOffset(java.time.Instant.now())).hour
+        time.minute = LocalDateTime.ofEpochSecond(dateTime!!, 0, java.time.ZoneId.systemDefault().rules.getOffset(java.time.Instant.now())).minute
 
         //save
 
         binding.saveButton.setOnClickListener {
-            if (id!= null && name != null && added != null) {
+            if (id!= null && name != null) {
                 val newTitle = title.text.toString()
-
-                MainActivity().updateReminder(id, newTitle, added)
+                val newDateTime = LocalDateTime.of(date.year, date.month+1, date.dayOfMonth, time.hour, time.minute, 0)
+                val newDateTimeLong = newDateTime.atZone(java.time.ZoneId.systemDefault()).toEpochSecond()
+                MainActivity().updateReminder(id, newTitle, newDateTimeLong)
                 MainActivity().loadData().run {
                     //refresh the list
                     startActivity(Intent(this@EditActivity, MainActivity::class.java)).also { finish() }
---
+++ app/src/main/java/com/example/myapplication/services/NotificationBroadcastReceiver.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication.services
+
+import android.app.AlertDialog
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class NotificationBroadcastReceiver : BroadcastReceiver() {
+
+        override fun onReceive(context: Context?, intent: Intent?) {
+            // Handle the alarm event here
+            // You can show the AlertDialog or any other action you want
+            val alertDialog = AlertDialog.Builder(context)
+                .setTitle("Notification")
+                .setMessage("Time to wake up!")
+                .setPositiveButton("OK") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .create()
+            alertDialog.show()
+            //start notification service when date is reached
+            val service = Intent(context, NotificationService::class.java)
+            service.putExtra("reason", "notification")
+            service.putExtra("timestamp", intent?.getLongExtra("timestamp", 0))
+            service.putExtra("reminderBody", intent?.getStringExtra("reminderBody"))
+            context?.startService(service)
+
+        }
+
+
+}
