204d44119699bba590ce21286ad26078af37af6e
Axel Santoso <axcelsantoso0@gmail.com>
2023-09-01 14:31:33 +0700

    clear unused code

--- app/src/main/java/com/example/myapplication/services/AlarmManager.kt
+++ app/src/main/java/com/example/myapplication/services/AlarmManager.kt
@@ -3,13 +3,6 @@ package com.example.myapplication.services
 import java.util.Calendar
 
 class AlarmManager {
-    //make alarm manager
-//    fun setAlarm(context: Context, calendar: Calendar, id: Int, reminderBody: String) {
-//        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-//        val intent = Intent(context, NotificationBroadcastReceiver::class.java)
-//        intent.putExtra("timestamp", calendar.timeInMillis)
-//        intent.putExtra("reminderBody", reminderBody)
-//        val pendingIntent = PendingIntent.getBroadcast(context, id, intent, 0)
-//        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
-//    }
+
+
 }
