5c1fb7db4d7cde09cd41a20d82b4d582c29e7d79
Axel Santoso <axcelsantoso0@gmail.com>
2023-09-04 17:02:02 +0700

    clear unused code

--- app/src/main/java/com/example/myapplication/services/AlarmReceiver.kt
+++ app/src/main/java/com/example/myapplication/services/AlarmReceiver.kt
@@ -38,41 +38,6 @@ class AlarmReceiver : BroadcastReceiver() {
         val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.notify(notificationID, notification)
 
-        //start notification service when date is reached
-        val service = Intent(context, NotificationService::class.java)
-        service.putExtra("reason", "notification")
-        service.putExtra("timestamp", intent?.getLongExtra("timestamp", 0))
-        service.putExtra("reminderBody", intent?.getStringExtra("reminderBody"))
-        context.startService(service)
-
-
-
-        //cancel notification when reminder is deleted
-        if(intent?.getStringExtra("reason") == "delete"){
-            notificationManager.cancel(notificationID)
-        }
-        else if(intent?.getStringExtra("reason") == "notification"){
-            val alertDialog = AlertDialog.Builder(context)
-                .setTitle("Notification")
-                .setMessage("Time to wake up!")
-                .setPositiveButton("OK") { dialog, _ ->
-                    dialog.dismiss()
-                }
-                .create()
-            alertDialog.show()
-        }
-        else if(intent?.getStringExtra("reason") == "snooze") {
-            val alertDialog = AlertDialog.Builder(context)
-                .setTitle("Notification")
-                .setMessage("Time to wake up!")
-                .setPositiveButton("OK") { dialog, _ ->
-                    dialog.dismiss()
-                }
-                .create()
-            alertDialog.show()
-        }
-
-
 
     }
 
