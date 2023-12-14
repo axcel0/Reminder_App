f70c8b648df85eb7ed25cf84b713ab936e2cc40c
Axel Santoso <axcelsantoso0@gmail.com>
2023-12-14 09:31:07 +0700

    migrate to android junit 5 for testing.

--- app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.kt
@@ -1,19 +1,9 @@
 package com.example.myapplication
 
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
 
-import org.junit.Test
-import org.junit.runner.RunWith
-
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-@RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
